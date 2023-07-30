5086f796746562f54ee81e799d4288447f4ef880
Asadullah Rifat <anonymusrif@gmail.com>
2023-07-30 13:31:26 +0600

    entity base and user class added

---
+++ IdentityCore/Models/Entities/EntityBase.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityCore.Models.Entities
+{
+    [BsonIgnoreExtraElements]
+    public class EntityBase
+    {
+        [BsonId]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+
+        public DateTime? ModifiedOn { get; set; } = null;
+
+        public string CreatedBy { get; set; } = string.Empty;
+
+        public string ModifiedBy { get; set; } = string.Empty;
+    }
+}
---
+++ IdentityCore/Models/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace IdentityCore.Models.Entities
+{
+    public class User : EntityBase
+    {
+        public string Password { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string[] Claims { get; set; } = new string[0];
+    }
+}
