a263237ae0fdf6975400872bcc471eada0e3e797
Asadullah Rifat <anonymusrif@gmail.com>
2023-08-05 15:39:45 +0600

    upload blob file command handler and validatior added

---
+++ src/BlobCore/Implementations/Commands/Validators/UploadBlobFileCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using BlobCore.Declarations.Commands;
+using BlobCore.Declarations.Repositories;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlobCore.Implementations.Commands.Validators
+{
+    public class UploadBlobFileCommandValidator : AbstractValidator<UploadBlobFileCommand>
+    {
+        private readonly IBlobFileRepository _blobFileRepository;
+
+        public UploadBlobFileCommandValidator(IBlobFileRepository blobFileRepository)
+        {
+            _blobFileRepository = blobFileRepository;
+
+            RuleFor(x => x.FormFile).NotNull();
+            RuleFor(x => x).Must(x => x.FormFile.Length > 0).When(x => x.FormFile != null);
+            RuleFor(x => x.FormFile.Name).NotNull().NotEmpty().When(x => x.FormFile != null);
+            RuleFor(x => x.FormFile.ContentType).NotNull().When(x => x.FormFile != null);
+        }
+    }
+}
