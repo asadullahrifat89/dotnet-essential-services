6fd99b4327c39940e76fa5977f733d2271999bcd
Asadullah Rifat <anonymusrif@gmail.com>
2023-08-09 00:11:55 +0600

    LingoCore added

---
+++ src/LingoCore/Models/Entities/LingoApp.cs
@@ -0,0 +1,9 @@
+﻿using BaseCore.Models.Entities;
+
+namespace LingoCore.Models.Entities
+{
+    public class LingoApp : EntityBase
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ src/LingoCore/Models/Entities/LingoResource.cs
@@ -0,0 +1,15 @@
+﻿using BaseCore.Models.Entities;
+
+namespace LingoCore.Models.Entities
+{
+    public class LingoResource : EntityBase
+    {
+        public string AppId { get; set; } = string.Empty;
+
+        public string LanguageCode { get; set; } = string.Empty;
+
+        public string ResourceKey { get; set; } = string.Empty;
+
+        public string ResourceValue { get; set; } = string.Empty;
+    }
+}
