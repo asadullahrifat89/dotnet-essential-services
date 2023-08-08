d61678890e1396f3b7f1639d4f1c30253e7e4450
Saimon Siddique <139350786+SaimonSelise@users.noreply.github.com>
2023-08-09 01:11:20 +0600

    declerations and implementations folders  with command, query, repository files added

---
+++ src/LingoCore/Implementations/Queries/Validators/GetLingoAppQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LingoCore.Implementations.Queries.Validators
+{
+    public class GetLingoAppQueryValidator
+    {
+
+    }
+}
