40e5804563d9717086d0bacd6206036ba042edb8
Pollock Nag <139329988+pollock-selise@users.noreply.github.com>
2023-08-10 12:58:39 +0600

    added ActivationRequests Entity in Indentity Core

---
+++ src/IdentityCore/Models/Entities/ActivationRequests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityCore.Models.Entities
+{
+    public class ActivationRequests
+    {
+        public string Email = string.Empty;
+        public string ActivationKey = string.Empty;
+        public ActivationStatus ActivationStatus = ActivationStatus.Expired;
+
+    }
+
+    public enum ActivationStatus
+    {
+        Activated ,
+        Expired 
+    }
+
+}
