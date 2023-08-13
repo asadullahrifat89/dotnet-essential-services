2825efe86d15a87d8f94dd7c1c106e38956f41bd
Asadullah Rifat <anonymusrif@gmail.com>
2023-08-13 23:14:52 +0600

    add command added

---
+++ src/TeamsCore/Declarations/Commands/AddProductCommand.cs
@@ -0,0 +1,34 @@
+﻿using BaseCore.Models.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamsCore.Models.Entities;
+
+namespace TeamsCore.Declarations.Commands
+{
+    public class AddProductCommand : IRequest<ServiceResponse>
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string ManPower { get; set; } = string.Empty;
+
+        public string Experience { get; set; } = string.Empty;
+
+        public string EmploymentType { get; set; } = string.Empty;
+
+        public ProductCostType ProductCostType { get; set; }
+
+        public string IconUrl { get; set; } = string.Empty;
+
+        public string BannerUrl { get; set; } = string.Empty;
+
+        public string[] LinkedSearchCriteriaIds { get; set; } = new string[0];
+
+        public string[] LinkedProjectIds { get; set; } = new string[0];
+    }
+}
