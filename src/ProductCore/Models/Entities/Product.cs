4c6a00f3846b8cde3a3fa91b70f6a2de3974eb30
Saimon Siddique <139350786+SaimonSelise@users.noreply.github.com>
2023-08-13 17:20:22 +0600

    ProductCore service and product entity added

---
+++ src/ProductCore/Models/Entities/Product.cs
@@ -0,0 +1,29 @@
+﻿using BaseCore.Models.Entities;
+
+namespace ProductCore.Models.Entities
+{
+    public class Product : EntityBase
+    {
+        public string ProductName { get; set; } = string.Empty;
+        public int ProductCost { get; set; } = 0;
+        public string ProductDescription { get; set; } = string.Empty;
+        public string ProductLogoName { get; set; } = string.Empty;
+        public ProductType ProductType { get; set; } = new ProductType();
+        public UpperLowerBound ManPower { get; set; } = new UpperLowerBound();
+        public UpperLowerBound Experience { get; set; } = new UpperLowerBound();
+
+        // Attachted Search Criteria Reference
+    }
+
+    public class ProductType
+    {
+        public string Key { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+    }
+
+    public class UpperLowerBound
+    {
+        public int Max { get; set; } = 0;
+        public int Min { get; set; } = 0;
+    }
+}
