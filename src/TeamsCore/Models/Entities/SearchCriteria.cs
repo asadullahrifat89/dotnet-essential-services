d19b38eed83b5410253f238a5158769fb76e9d8d
Asadullah Rifat <anonymusrif@gmail.com>
2023-08-13 22:08:30 +0600

    SearchCriteria entity done

---
+++ src/TeamsCore/Models/Entities/SearchCriteria.cs
@@ -0,0 +1,35 @@
+﻿using BaseCore.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamsCore.Models.Entities
+{
+    public class SearchCriteria : EntityBase
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Icon { get; set; } = string.Empty;
+
+        public SearchCriteriaType SearchCriteriaType { get; set; }
+
+        public SkillsetType SkillsetType { get; set; }
+    }
+
+    public enum SearchCriteriaType
+    {
+        Discipline,
+        Skillset,
+    }
+
+    public enum SkillsetType
+    {
+        Generic,
+        Hard,
+        Soft,
+    }
+}
