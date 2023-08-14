2e10d0e0cab4b996845d72def4ca57ad45c73842
Pollock Nag <139329988+pollock-selise@users.noreply.github.com>
2023-08-14 15:16:17 +0600

    added AddProject endpoint set

---
+++ src/TeamsCore/Declarations/Commands/AddProjectCommand.cs
@@ -0,0 +1,18 @@
+﻿using BaseCore.Models.Responses;
+using MediatR;
+
+namespace TeamsCore.Declarations.Commands
+{
+    public class AddProjectCommand : IRequest<ServiceResponse>
+    { 
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Link { get; set; } = string.Empty;
+
+        public string IconUrl { get; set; } = string.Empty;
+
+        public string[] LinkedProductIds { get; set; } = new string[0]; 
+    }
+}
---
+++ src/TeamsCore/Implementations/Commands/Validators/AddProjectCommandValidator.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamsCore.Declarations.Commands;
+using TeamsCore.Declarations.Repositories;
+using TeamsCore.Implementations.Repositories;
+
+namespace TeamsCore.Implementations.Commands.Validators
+{
+    public class AddProjectCommandValidator : AbstractValidator<AddProjectCommand>
+    {
+        private readonly IProductRepository _productRepository;
+        public AddProjectCommandValidator(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required.");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required.");
+            RuleFor(x => x.Link).NotEmpty().WithMessage("Link is required.");
+            RuleFor(x => x.IconUrl).NotEmpty().WithMessage("IconUrl is required.");
+
+            RuleFor(x => x.LinkedProductIds).NotEmpty().WithMessage("LinkedProductIds is required.");
+
+            // be an existing productID
+            RuleFor(x => x).MustAsync(BeAnExistingProductId).WithMessage("Product Id doesn't exist.").When(x => x.LinkedProductIds != null);
+        }
+
+        private async Task<bool> BeAnExistingProductId(AddProjectCommand command, CancellationToken arg2)
+        {
+            foreach (var productId in command.LinkedProductIds)
+            {
+                var exists = await _productRepository.BeAnExistingProductId(productId);
+
+                if (!exists)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
