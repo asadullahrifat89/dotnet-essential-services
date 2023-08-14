7e119e8febc23a357a65c5d8d9d31bd7965325eb
Pollock Nag <139329988+pollock-selise@users.noreply.github.com>
2023-08-14 15:17:43 +0600

    added get project endpoint set

---
+++ src/TeamsCore/Implementations/Queries/Handlers/GetProjectQueryHandler.cs
@@ -0,0 +1,56 @@
+﻿using BaseCore.Extensions;
+using BaseCore.Models.Responses;
+using BaseCore.Services;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamsCore.Declarations.Queries;
+using TeamsCore.Declarations.Repositories;
+using TeamsCore.Implementations.Queries.Validators;
+using TeamsCore.Models.Entities;
+
+namespace TeamsCore.Implementations.Queries.Handlers
+{
+    public class GetProjectQueryHandler : IRequestHandler<GetProjectQuery, QueryRecordResponse<Project>>
+    {
+        private readonly ILogger<GetProjectQueryHandler> _logger;
+        private readonly GetProjectQueryValidator _validator;
+        private readonly IProjectRepository _projectRepository;
+        private readonly IAuthenticationContextProvider _authenticationContext;
+
+        public GetProjectQueryHandler(
+            ILogger<GetProjectQueryHandler> logger,
+            GetProjectQueryValidator validator,
+            IProjectRepository projectRepository,
+            IAuthenticationContextProvider authenticationContext)
+        {
+            _logger = logger;
+            _validator = validator;
+            _projectRepository = projectRepository;
+            _authenticationContext = authenticationContext;
+        }
+
+
+        public async Task<QueryRecordResponse<Project>> Handle(GetProjectQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+                validationResult.EnsureValidResult();
+
+                return await _projectRepository.GetProject(request);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return Response.BuildQueryRecordResponse<Project>().BuildErrorResponse(
+                    Response.BuildErrorResponse().BuildExternalError(ex.Message, _authenticationContext.GetAuthenticationContext().RequestUri));
+            }
+        }
+    }
+
+}
---
+++ src/TeamsCore/Implementations/Queries/Validators/GetProjectQueryValidator.cs
@@ -0,0 +1,30 @@
+﻿using BaseCore.Extensions;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamsCore.Declarations.Queries;
+using TeamsCore.Declarations.Repositories;
+
+namespace TeamsCore.Implementations.Queries.Validators
+{
+    public class GetProjectQueryValidator : AbstractValidator<GetProjectQuery>
+    {
+        private readonly IProjectRepository _projectRepository;
+
+        public GetProjectQueryValidator(IProjectRepository projectRepository)
+        {
+            _projectRepository = projectRepository;
+
+            RuleFor(x => x.ProjectId).NotNull().NotEmpty();
+            RuleFor(x => x.ProjectId).MustAsync(BeAnExistingProject).WithMessage("Project doesn't exist.").When(x => !x.ProjectId.IsNullOrBlank());
+        }
+
+        private async Task<bool> BeAnExistingProject(string projectId, CancellationToken arg2)
+        {
+            return await _projectRepository.BeAnExistingProject(projectId);
+        }
+    }
+}
