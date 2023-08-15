aabddfb7805aab13380b7eb6f88349b02422e396
Asadullah Rifat <anonymusrif@gmail.com>
2023-08-16 05:20:53 +0600

    refact

---
+++ src/ProductSearchCriteriaModule/Application/Queries/GetProductSearchCriteriasQuery.cs
@@ -0,0 +1,14 @@
+﻿using BaseModule.Application.DTOs.Requests;
+using ProductSearchCriteriaModule.Domain.Entities;
+
+namespace ProductSearchCriteriaModule.Application.Queries
+{
+    public class GetProductSearchCriteriasQuery : PagedRequestBase<ProductSearchCriteria>
+    {
+        public string SearchTerm { get; set; } = string.Empty;
+
+        public ProductSearchCriteriaType? SearchCriteriaType { get; set; }
+
+        public SkillsetType? SkillsetType { get; set; }
+    }
+}
