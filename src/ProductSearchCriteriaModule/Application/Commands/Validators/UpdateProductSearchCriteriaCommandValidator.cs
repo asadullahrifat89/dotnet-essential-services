b77c4233efb8734fd8f79e4e8b02f503ba5c39fe
Pollock Nag <139329988+pollock-selise@users.noreply.github.com>
2023-08-16 18:45:19 +0600

    Fixed UpdateProductSearchCriteriaCommandValidator

--- src/ProductSearchCriteriaModule/Application/Commands/Validators/UpdateProductSearchCriteriaCommandValidator.cs
+++ src/ProductSearchCriteriaModule/Application/Commands/Validators/UpdateProductSearchCriteriaCommandValidator.cs
@@ -1,5 +1,6 @@
 ﻿using BaseModule.Infrastructure.Extensions;
 using FluentValidation;
+using ProductSearchCriteriaModule.Domain.Entities;
 using ProductSearchCriteriaModule.Domain.Repositories.Interfaces;
 
 namespace ProductSearchCriteriaModule.Application.Commands.Validators
@@ -15,8 +16,9 @@ namespace ProductSearchCriteriaModule.Application.Commands.Validators
             RuleFor(x => x.ProductSearchCriteriaId).NotNull().NotEmpty().WithMessage("Id must not be empty.");
             RuleFor(x => x.ProductSearchCriteriaId).MustAsync(BeAnExistingProductSearchCriteriaById).WithMessage("ProductSearchCriteria does not exist.").When(x => !x.ProductSearchCriteriaId.IsNullOrBlank());
 
-            RuleFor(x => x.ProductSearchCriteriaType).NotNull().NotEmpty().IsInEnum().WithMessage("ProductSearchCriteria Type is not acceptable.");
-            RuleFor(x => x.SkillsetType).NotNull().NotEmpty().IsInEnum().WithMessage("Skillset Type is not acceptable.");
+            RuleFor(x => x.ProductSearchCriteriaType).Must(x => x == ProductSearchCriteriaType.Discipline || x == ProductSearchCriteriaType.Skillset).WithMessage("ProductSearchCriteria Type is not acceptable.").When(x => x.ProductSearchCriteriaType != null);
+            RuleFor(x => x.SkillsetType).Must(x => x == SkillsetType.Soft || x == SkillsetType.Hard || x == SkillsetType.Generic).WithMessage("Skillset Type is not acceptable.").When(x => x.SkillsetType != null);
+
         }
 
         private async Task<bool> BeAnExistingProductSearchCriteriaById(string productSearchCriteriaId, CancellationToken token)
