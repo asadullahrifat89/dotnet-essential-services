6f4874e9ccbd2858fd8f44a98a8ecfa6bab34d77
Asadullah Rifat <anonymusrif@gmail.com>
2023-08-18 00:08:34 +0600

    refact

--- src/ContentMangement.Application/Commands/Validators/AddProductSearchCriteriaCommandValidator.cs
+++ src/ContentMangement.Application/Commands/Validators/AddProductSearchCriteriaCommandValidator.cs
@@ -16,12 +16,9 @@ namespace ContentMangement.Application.Commands.Validators
             RuleFor(x => x).MustAsync(NotBeAnExistingSearchCriteria).WithMessage("Name already exists.").When(x => !x.Name.IsNullOrBlank());
 
             RuleFor(x => x.Description).NotNull().NotEmpty().WithMessage("Description must not be empty");
-
             RuleFor(x => x.IconUrl).NotNull().NotEmpty().WithMessage("IconUrl must not be empty");
-
-            RuleFor(x => x.SearchCriteriaType).NotNull().NotEmpty().IsInEnum().WithMessage("SearchCriteria Type is not acceptable");
-
-            RuleFor(x => x.SkillsetType).NotNull().NotEmpty().IsInEnum().WithMessage("Skillset Type is not acceptable");
+            RuleFor(x => x.SearchCriteriaType).IsInEnum().WithMessage("SearchCriteria Type is not acceptable");
+            RuleFor(x => x.SkillsetType).IsInEnum().WithMessage("Skillset Type is not acceptable");
         }
 
         private async Task<bool> NotBeAnExistingSearchCriteria(AddProductSearchCriteriaCommand command, CancellationToken token)
