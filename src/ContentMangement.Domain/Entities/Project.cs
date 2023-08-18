17e189d5577707e0684f516f367c91cdb690624d
Asadullah Rifat <anonymusrif@gmail.com>
2023-08-18 15:09:39 +0600

    project entity added

---
+++ src/ContentMangement.Domain/Entities/Project.cs
@@ -0,0 +1,32 @@
+﻿using Base.Domain.Entities;
+
+namespace Teams.ContentMangement.Domain.Entities
+{
+    public class Project : EntityBase
+    {
+        /// <summary>
+        /// Name of the project.
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Detailed description of the project.
+        /// </summary>
+        public string Description { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Browsable link to the project.
+        /// </summary>
+        public string Link { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Icon image url of this project. It can be the company image for which this project is developed.
+        /// </summary>
+        public string IconUrl { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Array or image urls. It can contain screenshots of the project pages.
+        /// </summary>
+        public string[] ImageUrls { get; set; } = new string[] { };
+    }
+}
